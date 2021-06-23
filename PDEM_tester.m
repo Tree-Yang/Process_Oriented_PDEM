0f9277c208b005f2f7279ea4f8bef83528eb5d4d
J S Yang <yangjiashu2015@outlook.com>
2021-06-23 20:00:03 +0800

    Update the call of PDEM_solve function

--- PDEM_tester.m
+++ PDEM_tester.m
@@ -3,6 +3,8 @@ clear; clc; close all;
 %by J.S. Yang
 %date: 2019-11-05
 %representative point set
+%date: 2021-06-23
+%update the call of PDEM_solve function
 omega = (5*pi/4:pi/200:7*pi/4)';
 asgn_prob = [1/200, 1/100*ones(1, 99), 1/200]';
 np = length(omega);
@@ -19,7 +21,8 @@ for ii = 1:1:np
     v(:,ii) = -x0*omega(ii)*sin(omega(ii)*t0);
 end
 thres = inf;
-[tm, rm, prob] = PDEM_solve(asgn_prob, d, v, t0, dt, thres);
+dt_ratio = 1;
+[tm, rm, prob] = PDEM_solve(asgn_prob, d, v, t0, dt, dt_ratio, thres);
 %%
 PDEM_post(tm, rm, prob, 'surf', [0.9,1.1], [-0.2,0.2], 'Displacement[m]');
 PDEM_post(tm, rm, prob, 'contour', [0.9,1.1], [-0.2,0.2], 'Displacement[m]');
