91458edacf8f66d5a335ab2b14af075ee1082669
Yang, Jia-Shu <yangjiashu2015@outlook.com>
2024-11-24 19:28:40 +0800

    ADD REFERENCES INTO THE MAIN SCRIPT

--- PDEM_tester.m
+++ PDEM_tester.m
@@ -27,3 +27,11 @@ dt_ratio = 1;
 PDEM_post(tm, rm, prob, 'surf', [0.9,1.1], [-0.2,0.2], 'Displacement[m]');
 PDEM_post(tm, rm, prob, 'contour', [0.9,1.1], [-0.2,0.2], 'Displacement[m]');
 PDEM_post(tm, rm, prob, 'curve', [0.9,1.0,1.1], [-0.2,0.2], 'Displacement[m]');
+
+%% References:
+% -------------------------------------------------------------------------------
+% Li J, Chen JB. (2009) Stochastic dynamics of structures. Wiley, Singapore.
+% Li J, Chen JB. (2004) Probability density evolution method for dynamic response analysis of structures with uncertain parameters. Comput Mech 34(5): 400-409. http://link.springer.com/10.1007/s00466-004-0583-8
+% Chen JB, Yang JS, Jensen HA. (2020) Structural optimization considering dynamic reliability constraints via probability density evolution method and change of probability measure. Struct Multidisc Optim 62: 2499–2516. https://doi.org/10.1007/s00158-020-02621-4
+% Yang JS, Jensen HA, Chen JB. (2022) Structural optimization under dynamic reliability constraints utilizing probability density evolution method and metamodels in augmented input space. Struct Multidisc Optim 65: 107. https://doi.org/10.1007/s00158-022-03188-y
+% Yang JS, Chen JB, Jensen HA. (2022) Structural design optimization under dynamic reliability constraints based on the probability density evolution method and highly-efficient sensitivity analysis. Probab Eng Mech 68: 103205. https://doi.org/10.1016/j.probengmech.2022.103205
